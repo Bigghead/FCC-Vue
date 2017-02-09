dbd071112165ea5e281859c85f620fd288982297
Shav <shavenchew@yahoo.com>
2017-02-08 20:03:21 -0800

    Fighting with CSS!!! Fixed quote (center) and author (bottom right) of jumbotron

--- random-quote/src/App.vue
+++ random-quote/src/App.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="container">
-    <div class="quote-box">
-
+  <div class="container text-center">
+    <div class="heading ">
+      <h2>Random Quotes</h2>
     </div>
+
+    <quote-box
+    v-bind:quote="quote"
+    v-bind:author="author"
+    ></quote-box>
   </div>
 </template>
 
 <script>
+import Quotebox from './Quotebox.vue';
+
 export default {
-  name: 'app',
-  data () {
+
+  data: function(){
     return {
-      msg: 'Welcome to Your Vue.js App'
+      quote:  '"' + 'Random Words Go Inside Here ' + '"',
+      author: 'Some Cool Guy'
     }
+  },
+
+  components:{
+    'quote-box': Quotebox
   }
 }
 </script>
 
-<style scoped>
-
-  body{
-    min-height: 85vh;
-  }
+<style >
 
   .container{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+    margin-top: 5%;
   }
 
-  .quote-box {
-  height: 300px;
-  width: 50%;
-  border: 2px solid black;
-}
+
+
+
+
 </style>
---
+++ random-quote/src/Quotebox.vue
@@ -0,0 +1,49 @@
+<template id="">
+
+  <div class="">
+    <div class="jumbotron vertical-center">
+      <h2>{{ quote }}</h2>
+
+      <h4 class='item'>- {{ author }}</h4>
+    </div>
+    <div class="">
+      <button type="button" class='btn btn-primary'>New Quote</button>
+      <button type="button" class='btn btn-info'>Random Quote</button>
+    </div>
+    </div>
+  </div>
+
+</template>
+
+<script type="text/javascript">
+
+    export default{
+      props: ['quote', 'author']
+    }
+</script>
+
+<style scoped>
+.jumbotron{
+  margin:  auto;
+  height: 400px;
+  background: black;
+  color: white;
+}
+
+.vertical-center {
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  flex-wrap: nowrap;
+}
+
+.item{
+  align-self: flex-end;
+  transform: translateY(500%);
+}
+
+
+
+</style>
