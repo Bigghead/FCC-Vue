661ae6840738427329a3723cd061284a60d9c43f
Shav <shavenchew@yahoo.com>
2017-02-08 19:13:41 -0800

    Gave up on flexbox. Importing Bootstrap just to use a well like a G

---
+++ random-quote/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="quote-box">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  body{
+    min-height: 85vh;
+  }
+
+  .container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .quote-box {
+  height: 300px;
+  width: 50%;
+  border: 2px solid black;
+}
+</style>
