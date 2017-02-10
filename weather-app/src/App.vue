e5a43ff88c4f2a7ecb102d712783fc74651707d3
Shav <shavenchew@yahoo.com>
2017-02-09 22:05:57 -0800

    Promise chain for functions that depend on each other.

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -12,35 +12,45 @@
       return {
         lat: '',
         long: '',
-        done: false
-
+        weatherData: '',
+        i: 0
       }
     },
 
     methods:{
       getIP : function(){
         var vm = this;
-        axios.get('http://ip-api.com/json')
-          .then(function(geolocation){
-            var ipData = geolocation.data;
-            vm.lat = ipData.lat;
-            vm.long = ipData.lon;
-            return lat;
-          });
+
+        return new Promise(function(resolve, reject){
+           return axios.get('http://ip-api.com/json')
+            .then(function(geolocation){
+              var ipData = geolocation.data;
+              vm.lat = ipData.lat;
+              vm.long = ipData.lon;
+              resolve();
+            });
+        });
       },
 
       getWeatherData: function(lat, long){
         var vm = this;
-        axios.get('https://api.darksky.net/forecast/'+ Keys.darkSky + '/' + lat + ',' + long)
-          .then(function(weatherData){
-            console.log(weatherData);
-          });
+
+        return new Promise(function(resolve, reject){
+          return axios.get('https://api.darksky.net/forecast/'+ Keys.darkSky + '/' + lat + ',' + long)
+            .then(function(weatherData){
+              vm.weatherData = weatherData.request.responseText;
+              resolve();
+            });
+        });
       }
     },
     mounted: function(){
-      var vm = this;
-
-    }
+        var vm = this;
+        vm.getIP()
+            .then(function(){
+              return vm.getWeatherData(vm.lat, vm.long);
+            });
+      }
   }
 
 </script>
