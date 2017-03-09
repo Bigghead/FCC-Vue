4d41bf9c0e6c5001b3bd72c762de5737914a41c9
Shav <shavenchew@yahoo.com>
2017-03-08 22:32:37 -0800

    Figuring out CORS get isssues from a local file/localhost

--- Wikipedia-viewer/app.js
+++ Wikipedia-viewer/app.js
@@ -1,6 +1,16 @@
 var app = new Vue({
   el: '#root',
   data: {
-    title: 'Hello'
+    title: 'Wikipedia Viewer',
+    searchTerm: ''
+  },
+  methods:{
+    submit: function(e){
+      axios.get("http://en.wikipedia.org/w/api.php?action=opensearch&search=Nike&format=json")
+            .then(function(res){
+              console.log(res);
+            });
+            this.searchTerm = '';
+    }
   }
 });
